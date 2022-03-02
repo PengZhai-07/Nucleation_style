364d612a9d1568ed6df88840c005834b0b7a2d85
PengZhai <zhaipeng_std@163.com>
2022-03-02 20:40:59 +0800

    understand how to generate the 2D mesh

--- src/MeshBox.jl
+++ src/MeshBox.jl
@@ -17,6 +17,18 @@
 #			to top-right. The table iglob is needed to assemble 
 #			global data from local data.
 
+				# 29 33 37 41 45
+  				# 28 32 36 40 44
+				# 27 31 35 39 43
+		#ex     # 26 30 34 38 42
+				# 5 10 15 20 25 
+  				# 4 9 14 19 24 
+				# 3 8 13 18 23
+			    # 2 7 12 17 22
+				# 1 6 11 16 21
+				    # ey
+
+
 #			x[:] = global x coordinates of all GLL nodes, starting at 0
 #			y[:] = global y coordinates of all GLL nodes, starting at 0
 ###############################################################################
@@ -49,8 +61,8 @@ function MeshBox!(NGLL, Nel, NelX, NelY, FltNglob, dxe, dye)
 	xgll = dxe*xgll              # project to local coordinate
 
 
-	@inbounds for ey = 1:NelY         # number of y elements    row
-		@inbounds for ex = 1:NelX     # number of x elements    column
+	@inbounds for ey = 1:NelY         # number of Y elements  
+		@inbounds for ex = 1:NelX     # number of X elements  
 
 			et = et + 1
 
@@ -60,26 +72,28 @@ function MeshBox!(NGLL, Nel, NelX, NelY, FltNglob, dxe, dye)
 
 			# ig: index of GLL in each element
             
-			if et == 1
-				ig = reshape(collect(1:NGLL*NGLL), NGLL, NGLL)     
+			if et == 1   # the first element
+				ig = reshape(collect(1:NGLL*NGLL), NGLL, NGLL) 
+
 			else
-				if ey == 1       # Bottom Row
-					ig[1,:] = iglob[NGLL, :, et-1]   # Left edge
+				if ey == 1         
+					# et-1 is the last element, common GLL nodes on the boundary of elements
+					ig[1,:] = iglob[NGLL, :, et-1]   # Bottom edge
 					ig[2:end, :] = last_iglob .+ igL # The rest
 
-				elseif ex == 1	    # Left Column
-					ig[:,1] = iglob[:,NGLL,et-NelX]	# Bottom edge
+				elseif ex == 1	    
+					ig[:,1] = iglob[:,NGLL,et-NelX]	# Left edge  
 					ig[:,2:end] = last_iglob .+ igB 	# The rest
-
+				
 				else 			# Other Elements
-					ig[1,:] = iglob[NGLL, :, et-1]	# Left edge
-					ig[:,1] = iglob[:, NGLL, et-NelX]# Bottom edge
+					ig[1,:] = iglob[NGLL, :, et-1]	# bottom edge
+					ig[:,1] = iglob[:, NGLL, et-NelX]# Left edge
 					ig[2:end, 2:end] = last_iglob .+ igLB
 				end
 			end
 
 			iglob[:,:,et] = ig
-			last_iglob = ig[NGLL, NGLL]
+			last_iglob = ig[NGLL, NGLL]  # largest index of last element
 
 			# Global coordinates of computational nodes
 			# dimension of x and y is 1, index of x/y can be array, but the final result is still a vector seprately !!!
