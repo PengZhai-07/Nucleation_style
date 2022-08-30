129d948a6ec8bd4ab6661c4f901cf562917084fc
PengZhai <zhaipeng_std@163.com>
2022-08-30 18:36:43 -0400

    comment in run.jl

--- run.jl
+++ run.jl
@@ -19,7 +19,7 @@ include("$(@__DIR__)/par.jl")	    #	Set Parameters
 
 FZdepth = 24e3   # depth of fault zone  unit: m
 halfwidth = 500   # half width of damage zone   unit:m
-res = 8   # resolution of mesh
+res = 10   # resolution of mesh
 # 4: 481 GLL nodes, average 100m on fault  
 # 6: 721 GLL nodes, average 67m on fault
 # 8: 961 GLL nodes, average 50m on fault
@@ -36,8 +36,8 @@ alpha = 0.64    # initial(background) rigidity ratio: fault zone/host rock
 # runing time
 # case01: 17 hours on wozhi
 
-cos_reduction = 0.0396    # coseismic rigidity reduction 
-multiple = 3    # effective normal stress on fault: 10MPa*multiple  
+#cos_reduction = 0.0396    # coseismic rigidity reduction 
+#multiple = 3    # effective normal stress on fault: 10MPa*multiple  
 # cos_reduction = 0.0591    # coseismic rigidity reduction 
 # multiple = 5    # effective normal stress on fault: 10MPa*multiple
 cos_reduction = 0.0784    # coseismic rigidity reduction   
