541bd58ff3f90e07836214afdf710678a4defb50
PengZhai <zhaipeng_std@163.com>
2022-09-17 14:19:37 -0400

    submit

--- ratio_halfwidth_cycle.py
+++ ratio_halfwidth_cycle.py
@@ -15,11 +15,9 @@ list = ["#!/bin/bash\n# The interpreter used to execute the script\n\n",\
             "#SBATCH --mail-user=zhai5108@gmail.com\n",\
             "#SBATCH --mail-type=BEGIN,END\n",\
             "#SBATCH --nodes=1\n",\
-            "#SBATCH --mem=120000m\n",\
+            "#SBATCH --mem=60000m\n",\
             "#SBATCH --time=14-00:00:00\n",\
-            "#SBATCH --partition=standard\n\n",\
-            "#SBATCH --output=/home/%u/log/%x-%j.log\n",\
-            "#SBATCH --error=/home/%u/log/error-%x-%j.log\n\n"]
+            "#SBATCH --partition=standard\n\n"]
 
 par = pd.read_table("./key_par.txt", header = 0, sep=" ")
 print(par.columns)
@@ -29,32 +27,21 @@ halfwidth = par['halfwidth']
 L = par['L']
 co_size = par['co_size']
 
-N = 8    # number of tasks for each bash script
-n = 0
+n = 0              # number of tasks: calculate from the (n+1)th model
+N = 8              # threads for each task
 
-with open('ratio_halfwidth_cycle_part_1.sh','w') as f:       
+with open('ratio_halfwidth_cycle.sh','w') as f:       
     f.writelines(list)
-    f.write("#SBATCH --job-name=gl_case1-%.0f \n" %(N))
     f.write("#SBATCH --account=yiheh0\n")
-    f.write("#SBATCH --ntasks-per-node=32\n\n")
+    f.write("#SBATCH --ntasks-per-node=%.0f \n\n" %(N))           # request N cores
     f.write("# The application(s) to execute along with its input arguments and options:\n")
     f.write("# half-width(m) rigidity_ratio Lc(m)\n\n")
-    for i in range(0,N):
+    f.write("#Uncomment the following words to submit different simulations!!! \n\n\n")
+    for i in range(n,len(par)):              # here from task 2
             n = n+1
-            f.write("julia --threads 4 run.jl %.6f %.6f %.6f > /home/pengzhai/log/gl_case%.0f \n\n"  \
-                %(ratio.iloc[i],halfwidth.iloc[i],L.iloc[i],n))
-    f.write("echo 'Finish submitting!'")
+            f.write("##SBATCH --job-name=glcase%.0f \n"  %(n))
+            f.write("##SBATCH --output=/home/%u/log/%x-%j.log \n")
+            f.write("##SBATCH --error=/home/%u/log/error-%x-%j.log \n")
+            f.write("#julia --threads %.0f run.jl %.6f %.6f %.6f \n\n"\
+                %(N,ratio.iloc[i],halfwidth.iloc[i],L.iloc[i]))
 
-
-with open('ratio_halfwidth_cycle_part_2.sh','w') as f:       
-    f.writelines(list)
-    f.write("#SBATCH --job-name=gl_case%.0f-%.0f \n" %(N+1,len(par)))
-    f.write("#SBATCH --account=yiheh0\n")
-    f.write("#SBATCH --ntasks-per-node=32\n\n")
-    f.write("# The application(s) to execute along with its input arguments and options:\n")
-    f.write("# half-width(m) rigidity_ratio Lc(m)\n\n")
-    for i in range(N,len(par)):
-            n = n+1
-            f.write("julia --threads 4 run.jl %.6f %.6f %.6f > /home/pengzhai/log/gl_case%.0f \n\n"  \
-                %(ratio.iloc[i],halfwidth.iloc[i],L.iloc[i],n))
-    f.write("echo 'Finish submitting!'")
--- ratio_halfwidth_cycle.sh
+++ ratio_halfwidth_cycle.sh
@@ -1,46 +1,104 @@
 #!/bin/bash
 # The interpreter used to execute the script
 
-# This project is to explore the phase diagram (Ru=11) with 
-    # different rigidity ratio, halfwidth and characteristic slip distance 
+# This project is to explore the phase diagram (Ru=11) with
+# different rigidity ratio, halfwidth and characteristic slip distance 
 
-# 1 
-##“#SBATCH” directives that convey submission options:
-
-#SBATCH --mail-user=zhai5108@gmail.com
-#SBATCH --mail-type=BEGIN,END
-#SBATCH --nodes=1
-#SBATCH --cpus-per-task=4
-#SBATCH --time=14-00:00:00
-#SBATCH --partition=standard
-
-# The application(s) to execute along with its input arguments and options:
-#SBATCH --output=/home/%u/%x-%j.log
-#SBATCH --error=/home/%u/error-%x-%j.log
-
-# half-width(m) rigidity_ratio Lc(m)
-
-#SBATCH --job-name= glcase1 
-#SBATCH --account=yiheh0
-julia run.jl 0.500000 500.000000 0.010081 
-
-# 2 
-##“#SBATCH” directives that convey submission options:
+##SBATCH directives that convey submission options:
 
 #SBATCH --mail-user=zhai5108@gmail.com
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=4
+#SBATCH --mem=60000m
 #SBATCH --time=14-00:00:00
 #SBATCH --partition=standard
 
-# The application(s) to execute along with its input arguments and options:
-#SBATCH --output=/home/%u/%x-%j.log
-#SBATCH --error=/home/%u/error-%x-%j.log
+#SBATCH --account=yiheh0
+#SBATCH --ntasks-per-node=8 
 
+# The application(s) to execute along with its input arguments and options:
 # half-width(m) rigidity_ratio Lc(m)
 
-#SBATCH --job-name= gl-case2 
-#SBATCH --account=yiheh0
-julia run.jl 0.500000 1000.000000 0.011114 
+#Uncomment the following words to submit different simulations!!! 
+
+
+##SBATCH --job-name=glcase1 
+##SBATCH --output=/home/%u/log/%x-%j.log 
+##SBATCH --error=/home/%u/log/error-%x-%j.log 
+#julia --threads 8 run.jl 0.500000 500.000000 0.010081 
+
+##SBATCH --job-name=glcase2 
+##SBATCH --output=/home/%u/log/%x-%j.log 
+##SBATCH --error=/home/%u/log/error-%x-%j.log 
+#julia --threads 8 run.jl 0.500000 1000.000000 0.011114 
+
+##SBATCH --job-name=glcase3 
+##SBATCH --output=/home/%u/log/%x-%j.log 
+##SBATCH --error=/home/%u/log/error-%x-%j.log 
+#julia --threads 8 run.jl 0.500000 2000.000000 0.011343 
+
+##SBATCH --job-name=glcase4 
+##SBATCH --output=/home/%u/log/%x-%j.log 
+##SBATCH --error=/home/%u/log/error-%x-%j.log 
+#julia --threads 8 run.jl 0.500000 5000.000000 0.011350 
+
+##SBATCH --job-name=glcase5 
+##SBATCH --output=/home/%u/log/%x-%j.log 
+##SBATCH --error=/home/%u/log/error-%x-%j.log 
+#julia --threads 8 run.jl 0.333333 500.000000 0.014247 
+
+##SBATCH --job-name=glcase6 
+##SBATCH --output=/home/%u/log/%x-%j.log 
+##SBATCH --error=/home/%u/log/error-%x-%j.log 
+#julia --threads 8 run.jl 0.333333 1000.000000 0.016496 
+
+##SBATCH --job-name=glcase7 
+##SBATCH --output=/home/%u/log/%x-%j.log 
+##SBATCH --error=/home/%u/log/error-%x-%j.log 
+#julia --threads 8 run.jl 0.333333 2000.000000 0.017008 
+
+##SBATCH --job-name=glcase8 
+##SBATCH --output=/home/%u/log/%x-%j.log 
+##SBATCH --error=/home/%u/log/error-%x-%j.log 
+#julia --threads 8 run.jl 0.333333 5000.000000 0.017025 
+
+##SBATCH --job-name=glcase9 
+##SBATCH --output=/home/%u/log/%x-%j.log 
+##SBATCH --error=/home/%u/log/error-%x-%j.log 
+#julia --threads 8 run.jl 0.250000 500.000000 0.018327 
+
+##SBATCH --job-name=glcase10 
+##SBATCH --output=/home/%u/log/%x-%j.log 
+##SBATCH --error=/home/%u/log/error-%x-%j.log 
+#julia --threads 8 run.jl 0.250000 1000.000000 0.021856 
+
+##SBATCH --job-name=glcase11 
+##SBATCH --output=/home/%u/log/%x-%j.log 
+##SBATCH --error=/home/%u/log/error-%x-%j.log 
+#julia --threads 8 run.jl 0.250000 2000.000000 0.022673 
+
+##SBATCH --job-name=glcase12 
+##SBATCH --output=/home/%u/log/%x-%j.log 
+##SBATCH --error=/home/%u/log/error-%x-%j.log 
+#julia --threads 8 run.jl 0.250000 5000.000000 0.022700 
+
+##SBATCH --job-name=glcase13 
+##SBATCH --output=/home/%u/log/%x-%j.log 
+##SBATCH --error=/home/%u/log/error-%x-%j.log 
+#julia --threads 8 run.jl 0.200000 500.000000 0.022366 
+
+##SBATCH --job-name=glcase14 
+##SBATCH --output=/home/%u/log/%x-%j.log 
+##SBATCH --error=/home/%u/log/error-%x-%j.log 
+#julia --threads 8 run.jl 0.200000 1000.000000 0.027206 
+
+##SBATCH --job-name=glcase15 
+##SBATCH --output=/home/%u/log/%x-%j.log 
+##SBATCH --error=/home/%u/log/error-%x-%j.log 
+#julia --threads 8 run.jl 0.200000 2000.000000 0.028338 
+
+##SBATCH --job-name=glcase16 
+##SBATCH --output=/home/%u/log/%x-%j.log 
+##SBATCH --error=/home/%u/log/error-%x-%j.log 
+#julia --threads 8 run.jl 0.200000 5000.000000 0.028375 
 
