ab2b9b66d7a30a68339e8083336f359a0f2b4eef
PengZhai <zhaipeng_std@163.com>
2022-12-01 20:18:43 -0500

    domain size test

--- domain_size.sh
+++ domain_size.sh
@@ -13,8 +13,8 @@
 #SBATCH --time=14-00:00:00
 #SBATCH --partition=standard
 
-#SBATCH --account=yiheh0
-#SBATCH --ntasks-per-node=8 
+#SBATCH --account=yiheh1
+#SBATCH --ntasks-per-node=16 
 
 # The application(s) to execute along with its input arguments and options:
 # half-width(m) rigidity_ratio Lc(m) multiple(normal stress) cos_reduction
@@ -22,37 +22,8 @@
 #Uncomment the following words to submit different simulations!!! 
 
 
-##SBATCH --job-name=case41
-##SBATCH --output=/home/%u/log/%x-%j.log 
-##SBATCH --error=/home/%u/log/error-%x-%j.log 
-#julia --threads 8 run.jl 0.5 100 0.0070 5 0.00 
-
-##SBATCH --job-name=case42 
-##SBATCH --output=/home/%u/log/%x-%j.log 
-##SBATCH --error=/home/%u/log/error-%x-%j.log 
-#julia --threads 8 run.jl 0.5 200 0.0081 5 0.00 
-
-#SBATCH --job-name=case53 
+#SBATCH --job-name=case85 
 #SBATCH --output=/home/%u/log/%x-%j.log 
 #SBATCH --error=/home/%u/log/error-%x-%j.log 
-julia --threads 8 run.jl 0.5 500 0.010081 5 0.00  
+julia --threads 16 run.jl 0.8 500 0.01 4 0.00 0.019    
 
-##SBATCH --job-name=case52
-##SBATCH --output=/home/%u/log/%x-%j.log 
-##SBATCH --error=/home/%u/log/error-%x-%j.log 
-#julia --threads 8 run.jl 0.5 500 0.010081 5 0.00  
-
-##SBATCH --job-name=case44 
-##SBATCH --output=/home/%u/log/%x-%j.log 
-##SBATCH --error=/home/%u/log/error-%x-%j.log 
-#julia --threads 8 run.jl 0.5 1000 0.011114 5 0.00 
-
-##SBATCH --job-name=case45 
-##SBATCH --output=/home/%u/log/%x-%j.log 
-##SBATCH --error=/home/%u/log/error-%x-%j.log 
-#julia --threads 8 run.jl 0.5 2000 0.011343  5 0.00 
-
-##SBATCH --job-name=case46 
-##SBATCH --output=/home/%u/log/%x-%j.log 
-##SBATCH --error=/home/%u/log/error-%x-%j.log 
-#julia --threads 8 run.jl 0.5 5000 0.011350  5 0.00 
