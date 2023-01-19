96e1a34e4f659f958e82ead8b58d6dc336c6297b
PengZhai-07 <zhaipeng_std@163.com>
2023-01-19 16:26:55 -0500

    11

--- whole_space.sh
+++ whole_space.sh
@@ -6,10 +6,10 @@
 ##SBATCH --mail-user=pengzhai@umich.edu
 ##SBATCH --mail-type=FAIL,ARRAY_TASKS
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=8
+##SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16
 
-#SBATCH --mem=10000m
+#SBATCH --mem=20000m
 #SBATCH --time=14-00:00:00
 #SBATCH --partition=standard
 #SBATCH --account=yiheh1
@@ -17,5 +17,5 @@
 #SBATCH --output=/home/%u/log/%x-%j.log
 #SBATCH --error=/home/%u/log/error-%x-%j.log
 
-julia --threads 8 run.jl $SLURM_ARRAY_TASK_ID
+julia --threads 16 run.jl $SLURM_ARRAY_TASK_ID
 
