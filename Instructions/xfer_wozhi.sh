3f5aa7f99d7a60156c72711b27080590f16cd8a5
PengZhai-07 <zhaipeng_std@163.com>
2023-02-15 15:43:09 -0500

    111

--- Instructions/xfer_wozhi.sh
+++ Instructions/xfer_wozhi.sh
@@ -6,4 +6,6 @@ scp -r pengzhai@wozhi.earth.lsa.umich.edu:/home/pengzhai/Spear-normal-stress/dat
 # from Geatlakes to current working director
 scp -r pengzhai@greatlakes.arc-ts.umich.edu:/home/pengzhai/Spear-normal-stress/data/wholespace/phase_diagram_L_b/0_500_16_0.8_0.0_4_0.75_0.021429_0.0* /Users/pengzhai/Desktop/Spear-normal-stress/data/wholespace/phase_diagram_L_b/ 
 
+# from turbo to current working director
 
+scp -r pengzhai@greatlakes.arc-ts.umich.edu:/nfs/turbo/lsa-yiheh/yiheh-mistorage/pengz/data/wholespace/phase_diagram_L_b/0.75_32_100_0_0_1.0_0.0_4_0.6_0.04 /Users/pengzhai/Desktop/Spear-normal-stress/data/wholespace/phase_diagram_L_b/
